06b1481465a95627b5f2589377d4737a917e44fd
Yusuf-max552 <82448349+Yusuf-max552@users.noreply.github.com>
2022-01-24 19:02:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-noseX = 0;
-noseY = 0;
+noseX= 0;
+noseY= 0;
 
 function preload(){
+    clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 }
 function setup(){
     canvas = createCanvas(300, 300);
@@ -15,9 +16,7 @@ function setup(){
 }
 function draw(){
     image(video, 0, 0, 300, 300);
-    fill(255, 0, 0);
-    stroke(255, 0, 0);
-    circle(noseX, noseY, 20);
+    image(clown_nose , noseX, noseY, 30, 30);
 }
 function take_snapshot(){
     save('myFilterImage.png');
