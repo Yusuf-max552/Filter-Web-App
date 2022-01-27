acb12faf8f51f89e4cc92bb8e1002addd967b14e
Yusuf-max552 <82448349+Yusuf-max552@users.noreply.github.com>
2022-01-27 15:39:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-noseX= 0;
-noseY= 0;
-
 function preload(){
-    clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 }
 function setup(){
     canvas = createCanvas(300, 300);
@@ -15,8 +11,6 @@ function setup(){
     poseNet.on('pose', gotPoses);
 }
 function draw(){
-    image(video, 0, 0, 300, 300);
-    image(clown_nose , noseX, noseY, 30, 30);
 }
 function take_snapshot(){
     save('myFilterImage.png');
@@ -29,8 +23,6 @@ function gotPoses(results)
     if(results.length > 0)
     {
     console.log(results);
-    noseX = results[0].pose.nose.X;
-    noseY = results[0].pose.nose.y;
     console.log("nose x = " +results[0].pose.nose.x);
     console.log("nose y =" +results[0].pose.nose.y);
 }
